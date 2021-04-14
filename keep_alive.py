9b07445d2d4f86b12679d8d858e9352f2b9e2caa
Sitiaro <46511306+Sitiaro@users.noreply.github.com>
2021-04-14 19:26:38 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,3 +5,12 @@ app = Flask('')
 
 @app.route('/)
 def home():
+  return('ping')
+
+def run():
+  app.run(host='127.0.0.1', port=8080)
+           
+def keep_alive():
+  t=Thread(target=run)
+  t.start()
+           
